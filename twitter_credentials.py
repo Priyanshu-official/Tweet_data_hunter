6b9fb15cedf8060f6ea8f38d719d2a7f84aa19fe
rexdivakar <rexdivakar@hotmail.com>
2020-09-28 15:50:15 +0530

    Adding twitter data pull

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = "<access key>"
+ACCESS_TOKEN_SECRET = "<secret token>"
+CONSUMER_KEY = "<consumer key>"
+CONSUMER_SECRET = "<consumer secret token>"
